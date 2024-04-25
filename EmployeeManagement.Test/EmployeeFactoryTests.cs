c64e9c3e0ad1088ed1207e0ca4ab5cd38b1b4022
Rocío Lapeira Naranjo <r.lapeira.naranjo@avanade.com>
2024-04-25 11:39:54 +0200

    Demo - Writing Your First Unit Test

---
+++ EmployeeManagement.Test/EmployeeFactoryTests.cs
@@ -0,0 +1,18 @@
+﻿using EmployeeManagement.Business;
+using EmployeeManagement.DataAccess.Entities;
+
+namespace EmployeeManagement.Test
+{
+    public class EmployeeFactoryTests
+    {
+        [Fact]
+        public void CreateEmployee_ConstructInternalEmployee_SalaryMustBe2500()
+        {
+            var employeeFactory = new EmployeeFactory();
+
+            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Kevin", "Dockx");
+
+            Assert.Equal(5000, employee.Salary);
+        }
+    }
+}
