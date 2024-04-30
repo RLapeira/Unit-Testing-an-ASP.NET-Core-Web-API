1ae7cd660579f7cccffd29a6a4a816ec4386146a
Rocío Lapeira Naranjo <r.lapeira.naranjo@avanade.com>
2024-04-30 12:19:20 +0200

    Demo - Configuring Mock Object Return Values

--- EmployeeManagement.Test/MoqTests.cs
+++ EmployeeManagement.Test/MoqTests.cs
@@ -1,4 +1,5 @@
 ﻿using EmployeeManagement.Business;
+using EmployeeManagement.DataAccess.Entities;
 using EmployeeManagement.Services.Test;
 using Moq;
 
@@ -31,13 +32,34 @@ namespace EmployeeManagement.Test
             // Arrange
             var employeeManagementTestDataRepository = new EmployeeManagementTestDataRepository();
             var employeeFactoryMock = new Mock<EmployeeFactory>();
+            employeeFactoryMock.Setup(m =>
+                m.CreateEmployee(
+                    "Kevin",
+                    It.IsAny<string>(),
+                    null,
+                    false))
+                .Returns(new InternalEmployee("Kevin", "Dockx", 5, 2500, false, 1));
+            employeeFactoryMock.Setup(m =>
+                m.CreateEmployee(
+                    "Sandy",
+                    It.IsAny<string>(),
+                    null,
+                    false))
+                .Returns(new InternalEmployee("Sandy", "Dockx", 0, 3000, false, 1));
+            employeeFactoryMock.Setup(m =>
+                m.CreateEmployee(
+                    It.Is<string>(value => value.Contains("a")),
+                    It.IsAny<string>(),
+                    null,
+                    false))
+                .Returns(new InternalEmployee("SomeoneWithAna", "Dockx", 0, 3000, false, 1));
             var employeeService = new EmployeeService(
                 employeeManagementTestDataRepository,
                 employeeFactoryMock.Object);
 
             // suggested bonus for new employees = 
             // (years in service if > 0) * attended courses * 100
-            decimal suggestedBonus = 200;
+            decimal suggestedBonus = 1000;
 
             // Act
             var employee = employeeService.CreateInternalEmployee("Kevin", "Dockx");
