ad9185dceac3c99d179bba650cc3879e4ea7f4aa
Rocío Lapeira Naranjo <r.lapeira.naranjo@avanade.com>
2024-04-30 09:32:57 +0200

    Demo - Getting Data from an External Source

---
+++ EmployeeManagement.Test/TestData/StronglyTypedEmployeeServiceTestData_FromFile.cs
@@ -0,0 +1,21 @@
+﻿namespace EmployeeManagement.Test.TestData
+{
+    public class StronglyTypedEmployeeServiceTestData_FromFile : TheoryData<int, bool>
+    {
+        public StronglyTypedEmployeeServiceTestData_FromFile()
+        {
+            var testDataLines = File.ReadAllLines("TestData/EmployeeServiceTestData.csv");
+
+            foreach (var line in testDataLines)
+            {
+                var splitString = line.Split(',');
+                if (int.TryParse(splitString[0], out int raise) &&
+                    bool.TryParse(splitString[1], out bool minimunRaiseGiven))
+                {
+                    // Add test data
+                    Add(raise, minimunRaiseGiven);
+                }
+            }
+        }
+    }
+}
