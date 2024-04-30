341d97b5af39ad8bb0ab3863434a181109495c95
Rocío Lapeira Naranjo <r.lapeira.naranjo@avanade.com>
2024-04-30 10:57:35 +0200

    Demo - Creating and Using a Mock Object

---
+++ EmployeeManagement.Test/MoqTests.cs
@@ -0,0 +1,50 @@
+﻿using EmployeeManagement.Business;
+using EmployeeManagement.Services.Test;
+using Moq;
+
+namespace EmployeeManagement.Test
+{
+    public class MoqTests
+    {
+        [Fact]
+        public void FetchInternalEmployee_EmployeeFetched_SuggestedBonusMustBeCalculated()
+        {
+            // Arrange
+            var employeeManagementTestDataRepository = new EmployeeManagementTestDataRepository();
+            //var employeeFactory = new EmployeeFactory();
+            var employeeFactoryMock = new Mock<EmployeeFactory>();
+            var employeeService = new EmployeeService(
+                employeeManagementTestDataRepository,
+                employeeFactoryMock.Object);
+
+            // Act
+            var employee = employeeService.FetchInternalEmployee(
+                Guid.Parse("72f2f5fe-e50c-4966-8420-d50258aefdcb"));
+
+            // Assert
+            Assert.Equal(400, employee.SuggestedBonus);
+        }
+
+        [Fact]
+        public void CreateInternalEmployee_InternalEmployeeCreated_SuggestedBonusMustBeCalculated()
+        {
+            // Arrange
+            var employeeManagementTestDataRepository = new EmployeeManagementTestDataRepository();
+            var employeeFactoryMock = new Mock<EmployeeFactory>();
+            var employeeService = new EmployeeService(
+                employeeManagementTestDataRepository,
+                employeeFactoryMock.Object);
+
+            // suggested bonus for new employees = 
+            // (years in service if > 0) * attended courses * 100
+            decimal suggestedBonus = 200;
+
+            // Act
+            var employee = employeeService.CreateInternalEmployee("Kevin", "Dockx");
+
+            // Assert
+            Assert.Equal(suggestedBonus, employee.SuggestedBonus);
+        }
+
+    }
+}
