911532472c5840fa85237d00c18289bd2df34dc9
Rocío Lapeira Naranjo <r.lapeira.naranjo@avanade.com>
2024-05-01 16:24:58 +0200

    Demo - Verifying Model Types when Testing

--- EmployeeManagement.Test/InternalEmployeeControllerTests.cs
+++ EmployeeManagement.Test/InternalEmployeeControllerTests.cs
@@ -8,30 +8,51 @@ namespace EmployeeManagement.Test
 {
     public class InternalEmployeeControllerTests
     {
-        [Fact]
-        public async Task GetInternalEmployees_GetAction_MustReturnOkObjectResult()
+        private readonly InternalEmployeesController _internalEmployeesController;
+
+        public InternalEmployeeControllerTests()
         {
-            // Arrange
             var employeeServiceMock = new Mock<IEmployeeService>();
             employeeServiceMock
                 .Setup(m => m.FetchInternalEmployeesAsync())
-                .ReturnsAsync(new List<InternalEmployee>()
-                {
+                .ReturnsAsync(new List<InternalEmployee>() {
                     new InternalEmployee("Megan", "Jones", 2, 3000, false, 2),
                     new InternalEmployee("Jaimy", "Johnson", 3, 3400, true, 1),
                     new InternalEmployee("Anne", "Adams", 3, 4000, false, 3)
                 });
 
-            var internalEmployeesController = 
-                new InternalEmployeesController(employeeServiceMock.Object, null);
+            _internalEmployeesController = new InternalEmployeesController(
+                 employeeServiceMock.Object, null);
+        }
+
+        [Fact]
+        public async Task GetInternalEmployees_GetAction_MustReturnOkObjectResult()
+        {
+            // Arrange
 
             // Act
-            var result = await internalEmployeesController.GetInternalEmployees();
+            var result = await _internalEmployeesController.GetInternalEmployees();
 
             // Assert
             var actionResult = Assert
                 .IsType<ActionResult<IEnumerable<Models.InternalEmployeeDto>>>(result);
             Assert.IsType<OkObjectResult>(actionResult.Result);
+        }
+
+        [Fact]
+        public async Task GetInternalEmployees_GetAction_MustReturnIEnumerableOfInternalEmployeeDtoAsModelType()
+        {
+            // Arrange
+
+            // Act
+            var result = await _internalEmployeesController.GetInternalEmployees();
+
+            // Assert
+            var actionResult = Assert
+                .IsType<ActionResult<IEnumerable<Models.InternalEmployeeDto>>>(result);
+
+            Assert.IsAssignableFrom<IEnumerable<Models.InternalEmployeeDto>>(
+                ((OkObjectResult)actionResult.Result).Value);
         }
     }
 }
